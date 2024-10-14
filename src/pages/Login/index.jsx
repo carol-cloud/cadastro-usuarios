61619611c135ef5b3b24cd1b933594176be7131e
Ana Carolina <karols.15@hotmail.com>
2024-10-13 22:17:54 -0300

    feat: creating login submit handle

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,15 +1,41 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 import jpIMG from "../../assets/jp.svg";
 import { LayoutComponents } from "../../components/LayoutComponents/index";
+import api from "../../services/api";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    try {
+      const response = await api.post("/user/login", { email, senha: password });
+  
+      const data = response.data;
+      if (response.status === 200) {
+        localStorage.setItem("token", data.token);
+  
+        navigate("/dashboard");
+      } else {
+        setError(data.error || "Erro ao fazer login.");
+      }
+    } catch (err) {
+      setError(err.response?.data?.error || "Erro no servidor. Tente novamente mais tarde.");
+    }
+  }
+
   return (
     <LayoutComponents>
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleLogin}>
         <span className="login-form-title"> Bem vindo </span>
 
         <span className="login-form-title">
@@ -36,6 +62,7 @@ export const Login = () => {
           <span className="focus-input" data-placeholder="Password"></span>
         </div>
 
+        {error && <p style={{ color: "red" }}>{error}</p>} 
         <div className="container-login-form-btn">
           <button className="login-form-btn">Login</button>
         </div>
